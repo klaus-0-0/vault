e91075bed08a159ea7c4dbdf49912f15a76106f6
klaus-0-0 <jin0667666@gmail.com>
2025-10-06 17:32:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(), react()
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: true,
+    allowedHosts: ['vault-front-bas3.onrender.com'], 
+  },
 })
